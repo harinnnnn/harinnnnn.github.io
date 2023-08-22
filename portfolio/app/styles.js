b53283a6d50d90bce6dd3cc75c777fb5427bebfc
harinnn <harinnnkim@gmail.com>
2023-08-22 22:00:41 +0900

    [EDIT] main (대략적인 section 구성)

---
+++ portfolio/app/styles.js
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const Main = styled.main``;
+export const Section = styled.section`
+  min-height: 1020px;
+  height: fit-content;
+  border-bottom: 1px solid red;
+
+  & > h1 {
+    font-size: 50px;
+    font-weight: bold;
+  }
+`;
